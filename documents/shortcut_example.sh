b663542940af956fe01a9a8a7fa7b297db0b1b89
Mitchell Urgero <mitchell@urgero.org>
2025-04-22 17:04:08 -0500

    update example scriptlet

--- documents/shortcut_example.sh
+++ documents/shortcut_example.sh
@@ -4,4 +4,10 @@
 # Author: slyhype
 # DontUseFBInk
 
+## Last modified bits by yeetyeeter456 on Discord
+# Modify the "Last-opened" comment to set the current time
+# It's done in order to place the scriptlet to the top of recently read booklist
+timestamp=$(date +%s)
+sed -i "s/^# Last-opened:.*/# Last-opened: $timestamp/" "$0"
+
 /bin/bash /mnt/us/documents/shortcut_browser.sh -u "https://example.com"
